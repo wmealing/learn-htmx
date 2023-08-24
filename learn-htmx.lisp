1f9fa2b2df82de7ad9ffdb77736e91934c94f729
Wade Mealing <wmealing@gmail.com>
2023-08-25 01:52:05 +1000

    Minor fixes to match web 1.0 example.

--- learn-htmx.lisp
+++ learn-htmx.lisp
@@ -12,7 +12,6 @@
 (add-contact '(:first "Wade" :last "Mealing" :phone "5552345" :email "wmealing@wmealing.com"))
 
 
-
 (defvar *template-dir*
   (asdf:system-relative-pathname "learn-htmx" "templates/"))
 
@@ -32,26 +31,16 @@
       (read-sequence template template-file)
       (funcall (cl-template:compile-template template) values))))
 
-(easy-routes:defroute root ("/" :method :get) ()
-  (cl-who:with-html-output-to-string (s)
-    (:body
-     (:p "Hello world!"))))
-
-(defun match-contact (q-value)
-  (format t "Q VAL: |~A|~%" q-value)
-  (case q-value
-    ('nil *db*)
-    (otherwise (select-by-contact q-value)))
-  )
-
 (defun select-by-contact (contact-name)
-  (remove-if-not
-   #'(lambda (contact) (equal (getf contact :first) contact-name ))))
-
+  (if (string= contact-name "")
+      *db*
+      (remove-if-not
+       #'(lambda (contact) (equal (getf contact :first) contact-name ))*db*)
+      ))
 
 (easy-routes:defroute contacts ("/contacts" :method :get) (q)
-  (format t "Q IS: |~A|~%" q)
-  (apply-template "index.html" (list :people (match-contact q))))
+  (apply-template "index.html" (list :people (select-by-contact q)))
+  )
 
 (defun start-server (&key (port *port*))
   (format t "~&Starting the web server on port ~a" port)
