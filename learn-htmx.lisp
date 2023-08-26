90be68387ade0febc7354af6b2a949e8d1002790
Wade Mealing <wmealing@gmail.com>
2023-08-27 00:57:15 +1000

    Database updating functions.

--- learn-htmx.lisp
+++ learn-htmx.lisp
@@ -6,14 +6,49 @@
   "Server instance (Hunchentoot acceptor).")
 
 (defvar *db* nil)
-
 (setq *db* nil)
+
+(defvar *next* 3)
+
+(defun get-next ()
+  (setq *next* (+ *next* 1))
+  *next*)
 
 (defun add-contact (contact) (push contact *db*))
 
 (defun setup ()
   (add-contact '(:id "0" :first_name "Athene" :last_name "Chan" :phone "5551234" :email "achan@achan.com"))
   (add-contact '(:id "1" :first_name "Wade"   :last_name "Mealing" :phone "5552345" :email "wmealing@wmealing.com")))
+
+
+;; stolen from gigamonkeys.
+(defun select (selector-fn)
+  (remove-if-not selector-fn *db*))
+
+(defun where (&key id first_name last_name phone email)
+  #'(lambda (record)
+      (and
+       (if id          (equal (getf record :id)  id)  t)
+       (if first_name  (equal (getf record :first_name) first_name) t)
+       (if last_name   (equal (getf record :last_name) last_name) t)
+       (if email       (equal (getf record :last_name) email) t)
+       (if phone       (equal (getf record :phone) phone) t))))
+
+(defun update (selector-fn &key id first_name last_name phone email)
+  (setf *db*
+        (mapcar
+         #'(lambda (row)
+             (when (funcall selector-fn row)
+               (if id    (setf (getf row :id) id))
+               (if first_name   (setf (getf row :first_name) first_name))
+               (if last_name   (setf (getf row :last_name) last_name))
+               (if email   (setf (getf row :email) email))
+               (if phone   (setf (getf row :phone) phone)))
+             row) *db*)))
+
+(defun delete-rows (selector-fn)
+  (setf *db* (remove-if selector-fn *db*)))
+
 
 (defvar *template-dir*
   (asdf:system-relative-pathname "learn-htmx" "templates/"))
@@ -36,60 +71,53 @@
   (if (or (string= contact-name "")
           (eq contact-name 'nil))
       *db*
-      (remove-if-not
-       #'(lambda (contact) (equal (getf contact :first_name) contact-name ))*db*)
-      ))
+      (select (where :first_name contact-name))))
 
 (defun select-by-contact-id (id)
   (if (or (string= id "")
           (eq id 'nil))
       *db*
-      (remove-if-not
-       #'(lambda (contact) (equal (getf contact :id) id ))*db*)
-      ))
+      (select (where :id id))))
 
-(easy-routes:defroute index-contacts ("/contacts" :method :get) (q)
-  (apply-template "index.html" (list :people (select-by-contact q))))
+(easy-routes:defroute index-contacts ("/contacts" :method :get) ()
+  (apply-template "index.html" (list :people *db*)))
 
 (easy-routes:defroute contact-new-get ("/contacts/new" :method :get) ()
-  (apply-template "new-contact.html" (list :person '(:id 1 :first_name "Wade"))))
+  (apply-template "new-contact.html" (list :person nil)))
 
 ;; TODO use some validation magic here, return errors.
 (defun validate-contact (contact)
-  ;;  (list :first_name "Invalid first name , dawg")
-  '()
+  (declare (ignore contact))
+  'nil
   )
 
 (easy-routes:defroute contact-new-post ("/contacts/new" :method :post) ()
-  (progn
-    (let* ((email (hunchentoot:post-parameter "email"))
-           (first_name (hunchentoot:post-parameter "first_name"))
-           (last_name (hunchentoot:post-parameter "last_name"))
-           (phone (hunchentoot:post-parameter "phone"))
-           (next-id "10")
-           (contact `(:email      ,email
-                      :first_name ,first_name
-                      :last_name  ,last_name
-                      :phone      ,phone
-                      :id         ,next-id))
-           (errors (validate-contact contact)))
+  (let* ((email (hunchentoot:post-parameter "email"))
+         (first_name (hunchentoot:post-parameter "first_name"))
+         (last_name (hunchentoot:post-parameter "last_name"))
+         (phone (hunchentoot:post-parameter "phone"))
+         (contact `(:email      ,email
+                    :first_name ,first_name
+                    :last_name  ,last_name
+                    :phone      ,phone
+                    :id         ,(write-to-string (get-next))))
+         (errors (validate-contact contact)))
+    (format t "HAS ERRORS: ~A~%" errors)
 
-      (if (eq errors 'nil)
-          (progn
-            (add-contact contact) ;; maybe better error handling here ?
-            (hunchentoot:redirect "/contacts")            )
-          (progn
-            (apply-template "new-contact.html" (list :person contact :errors errors)))))))
+    (if (eq errors 'nil)
+        (progn
+          (add-contact contact) ;; maybe better error handling here ?
+          (hunchentoot:redirect "/contacts"))
+        (progn
+          (apply-template "new-contact.html" (list :person contact :errors errors))))))
 
 ;; view contact
 (easy-routes:defroute contact-view-get ("/contacts/:id" :method :get) ()
   (let ((contact (first (select-by-contact-id id))))
-    (format t "GOT CONTACT/:ID : ~s~%" (select-by-contact-id id))
     (apply-template "show.html" (list :contact contact))))
 
 (easy-routes:defroute contact-edit-screen-get ("/contacts/:id/edit" :method :get) ()
   (let ((contact (first (select-by-contact-id id))))
-    (format t "GOT CONTACT/:ID : ~s~%" (select-by-contact-id id))
     (apply-template "edit.html" (list :contact contact))))
 
 (easy-routes:defroute contact-edit-screen-post ("/contacts/:id/edit" :method :post) ()
@@ -104,13 +132,32 @@
                            :id         ,id)))
 
     ;; validate things here.
-    (add-contact posted-contact) ;; maybe better error handling here ?
+    ;;(add-contact posted-contact)
+
 
     (let ((errors (validate-contact posted-contact)))
 
       (if (not errors)
-          (apply-template "show.html" (list :contact posted-contact :errors errors))
-          (hunchentoot:redirect "/contacts")))))
+          ;; no errors update the contact
+          (progn
+            (update (where :id id) :email email :first_name first_name :phone phone)
+            (hunchentoot:redirect "/contacts"))
+          ;; otherwise show the errors.
+          (apply-template "show.html" (list :contact posted-contact :errors errors))))))
+
+(easy-routes:defroute contact-delete ("/contacts/:id/delete" :method :get) ()
+  (progn
+    (delete-rows (where :id id))
+    (hunchentoot:redirect "/contacts")))
+
+;; @app.route("/contacts/<contact_id>/delete", methods=["POST"]) (1)
+;; def contacts_delete(contact_id=0):
+;; contact = Contact.find(contact_id)
+;; contact.delete() (2)
+;; flash("Deleted Contact!")
+;; return redirect("/contacts") (3)
+
+
 
 (defun start-server (&key (port *port*))
   (format t "~&Starting the web server on port ~a" port)
